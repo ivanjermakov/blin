6c47064dea7870dfdb486492a77d637f0c73b4ca
ivanjermakov <ivanjermakov1@gmail.com>
2021-12-05 18:56:44 +0100

    Interactive version

--- src/control/repl.ts
+++ src/control/repl.ts
@@ -4,15 +4,6 @@ import {Pool} from '../core/pool'
 export class Repl {
 
 	pool: Pool
-	help = () => ({
-		availableCommands: [
-			{help: 'Show this message'},
-			{add: 'Add new peer [isMiner]'},
-			{del: 'Remove peer <id>'},
-			{show: 'Show peers [id]'},
-			{send: 'Send new message by peer <id> <type> <data>'},
-		]
-	})
 
 	constructor(pool: Pool) {
 		this.pool = pool
@@ -25,12 +16,10 @@ export class Repl {
 			ignoreUndefined: true
 		})
 		replServer.context.pool = this.pool
-		replServer.context.help = this.help
 	}
 
 	showMotd() {
-		console.log('Welcome to Blin v1.0.0')
-		console.log('Use /help for more information')
+		console.log(`Welcome to Blin ${process.env.npm_package_version}`)
 		console.log()
 	}
 
